0161e93fa9e525e8cbc58503501a65c0be4aba88
alxjsn <3642823+alxjsn@users.noreply.github.com>
2020-08-19 15:29:54 -0500

    Remove secretive from fish config

--- .config/fish/gorlami.fish
+++ .config/fish/gorlami.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
 set -gx NODE_PATH /usr/local/lib/node_modules/
-set -gx SSH_AUTH_SOCK /Users/alxjsn/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+set -gx PATH $PATH ~/Library/Python/3.8/bin /usr/local/Cellar/john-jumbo/1.9.0/share/john
