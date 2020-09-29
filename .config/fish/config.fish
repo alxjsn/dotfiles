bb76044c93da8a2d73d299c8fa7b6efcdbe078b7
alxjsn <3642823+alxjsn@users.noreply.github.com>
2020-09-29 16:11:24 -0500

    Update fish path to include ruby user installs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,14 +2,15 @@
 
 set fish_greeting
 
-set -gx EDITOR vim
-set -gx PATH ~/bin/(uname -n) ~/bin ~/go/bin/ ~/.local/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
+# Application settings
 set -gx GOPATH $HOME/go
-set -gx _JAVA_OPTIONS -Dswing.aatext=TRUE -Dawt.useSystemAAFontSettings=lcd
-set -gx TERM xterm-256color
-set -x GPG_TTY tty
 set -x GO111MODULE on
+set -gx TERM xterm-256color
+set -gx EDITOR vim
+set -x GPG_TTY tty
+set -gx _JAVA_OPTIONS -Dswing.aatext=TRUE -Dawt.useSystemAAFontSettings=lcd
 
+# Aliases
 alias pass="gopass"
 alias feh="feh  --sort filename --borderless --auto-zoom --scale-down --auto-rotate --image-bg black --draw-filename"
 alias scp="scp -F ~/.ssh/config"
@@ -17,6 +18,12 @@ alias rg="rg --no-ignore"
 alias jq="jq -C"
 alias less="less -R"
 alias ffuf='ffuf -c -ac -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4095.0 Safari/537.36"'
+
+# Paths
+set -gx PATH ~/bin/(uname -n) ~/bin ~/go/bin ~/.local/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
+if which ruby >/dev/null && which gem >/dev/null
+    set -gx PATH (ruby -r rubygems -e 'puts Gem.user_dir')/bin $PATH
+end
 
 # Source fish config based on hostnames
 if test -e ~/.config/fish/(uname -n).fish
