5a51e9a7a1fedba9f7f920dc9f528fdc60e64927
alxjsn <alxjsn@sameorigin.org>
2021-01-25 19:17:28 -0600

    Update fish ruby redirection

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,7 +22,7 @@ alias ffuf='ffuf -c -ac -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWeb
 set -gx PATH ~/bin/(uname -n) ~/bin ~/go/bin ~/.local/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
 
 # Add Ruby user dir to PATH
-if which ruby &>/dev/null && which gem &>/dev/null
+if which ruby 2>&1 && which gem 2>&1
     set -gx PATH (ruby -r rubygems -e 'puts Gem.user_dir')/bin $PATH
 end
 
